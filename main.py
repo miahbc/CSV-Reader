a0a34fe17841317f36b701722442f42b75ce68be
Miah Clay <miahb88@gmail.com>
2022-06-01 14:52:22 -0400

    working on it

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
- 
+# cats = open("cats.csv", "r")
+# dogs = open("dogs.csv", "r")
+# print(cats.read())
+# print(dogs.read())
+
+import csv
+
+# with open('cats.csv') as cats_file:
+#     csv_reader = csv.reader(cats_file, delimiter=',')
+#     line_count = 0
+#     for row in csv_reader:
+#         if line_count == 0:
+#             print(f"Column names are {', '.join(row)}")
+#             line_count += 1
+#         else:
+#             print(f'\t{row[0]} is a {row[1]}-year-old {row[2]}.')
+#             line_count += 1
+
+
+# with open('dogs.csv') as dogs_file:
+#     csv_reader = csv.reader(dogs_file, delimiter=',')
+#     line_count = 0
+#     for row in csv_reader:
+#         if line_count == 0:
+#             print(f"Column names are {', '.join(row)}")
+#             line_count += 1
+#         else:
+#             print(f'\t{row[0]} is a {row[1]}-year-old {row[2]}.')
+#             line_count += 1
+
+with open('cats.csv') as cats_file:
+    csv_reader = csv.reader(cats_file, delimiter=',')
+    catList = []
+    for row in csv_reader:
+        catList.append(row)
+    print(catList)
